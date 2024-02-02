55efdca159b51c14febbf02a5556e7c688db718b
Dushyant <117421735+Honey25609@users.noreply.github.com>
2024-02-02 20:17:51 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (8.37%) - LeetSync

--- 1212-sequential-digits/sequential-digits.cpp
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
-        vector<int> result;
-        int vec[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int lowLength = to_string(low).length();
-        int highLength = to_string(high).length();
+        vector<int> vec2;
+        int vec[9] = {1,2,3,4,5,6,7,8,9};
+        int x = to_string(low).length();
+        int y = to_string(high).length();
 
-        for (int length = lowLength; length <= highLength; ++length) {
-            for (int i = 0; i <= 9 - length; ++i) {
-                int number = 0;
-                for (int j = i; j < i + length; ++j) {
-                    number = number * 10 + vec[j];
-                }
 
-                if (number >= low && number <= high) {
-                    result.push_back(number);
+        for (int i = x-1; i < y; i++) {
+            for (int j = 0; j < 9-i; j++) {
+                int a = i;
+                long long b = 0;
+                int b2 = j;
+                while (a > -1 && b2 < 9) {
+                    b += vec[b2] * pow(10, a);
+                    b2++;
+                    a--;
                 }
+                a=i;
+
+            if (b > low-1 && b < high+1) {
+                vec2.push_back(b);
             }
         }
+    }   
 
-        return result;
+    return vec2;
     }
 };
