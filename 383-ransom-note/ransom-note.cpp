c4ca73952f2c3d3c28b833b3811e6fc491d57679
Dushyant <117421735+Honey25609@users.noreply.github.com>
2024-02-08 09:14:26 +0530

    Time: 14 ms (71.49%) | Memory: 11.2 MB (18.89%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -3,9 +3,10 @@ public:
     bool canConstruct(string ransomNote, string magazine) {
         unordered_map<char,int> map;
         int sum=0;
+        bool flag = false;
         for(int i=0;i<magazine.length();i++) map[magazine[i]]++;
         for(int i=0;i<ransomNote.length();i++){
-            if(map[ransomNote[i]]==0) return false;
+            if(map[ransomNote[i]]==0) return flag;
             map[ransomNote[i]]--;
         }        
         return true;
